a11265643d636e839b115bda5d6f7dd8d8b5b82c
TheHurricaneCat <thc12895nth@gmail.com>
2025-02-10 14:32:47 +0800

    refactored POS code and components

--- umai-pos-system/src/App.jsx
+++ umai-pos-system/src/App.jsx
@@ -1,34 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {initProductList} from '/src/handlers/DataHandler'
+import {products, modifiers} from '/src/handlers/Product.js'
+import useTrayManager from '/src/handlers/UseTrayManagers.js'
+
+import ProductCard from '/src/pos-components/ProductCard.jsx'
+import CategoryContainer from './pos-components/CategoryContainer'
+import TrayContainer from '/src/pos-components/TrayContainer.jsx'
+import KeypadContainer from '/src/pos-components/KeypadContainer.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // IMPORTANT NOTE:
+  // The following code is meant for the POS system
+  // Create new interfaces (i.e. new .jsx components) that will house the other interfaces 
 
+  const {
+    tray,
+    currentTray,
+    addNewTray,
+    addToTray,
+    handleProductIncrement,
+    handleProductDecrement,
+    handleProductDeletion,
+    addModifier,
+    handleModifierIncrement,
+    handleModifierDecrement,
+    handleModifierDeletion,
+    setCurrentTray,
+    currentProduct,
+    setCurrentProduct
+  } = useTrayManager();
+
+  let productList = initProductList(products);
+  let modifierList = initProductList(modifiers);
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    // interfaces are invisible containers that hold the components
+    // viewers are containers that displays a list of components (e.g. products, modifiers, trays)
+    <div className="primaryInterface">
+      <div className="productInterface"> 
+        <div className="productViewer"> 
+          {productList.map((category) => (
+            <CategoryContainer category={category} type="product" 
+            addToTray={addToTray}/>
+          ))}
+        </div>
+        <div className="modifierViewer">
+          {modifierList.map((category) => (
+            <CategoryContainer category={category} type="modifier"
+            addModifier={addModifier}
+             />
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="trayInterface">
+        <div className="trayViewer">
+          {tray.map((content) => (
+                <TrayContainer content={content} 
+                handleProductIncrement={handleProductIncrement} 
+                handleProductDecrement={handleProductDecrement} 
+                handleProductDeletion={handleProductDeletion} 
+                
+                handleModifierIncrement={handleModifierIncrement} 
+                handleModifierDecrement={handleModifierDecrement} 
+                handleModifierDeletion={handleModifierDeletion}
+                
+                setCurrentTray={setCurrentTray}
+                setCurrentProduct={setCurrentProduct} />
+          ))}
+        </div>
+        <div className="keypadViewer"> 
+          <KeypadContainer addNewTray={addNewTray}/>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ umai-pos-system/src/handlers/TrayDisplayManager.js
@@ -0,0 +1,16 @@
+import TrayContainer from "/src/pos-components/TrayContainer";
+
+export function TrayManager({ tray, handleProductIncrement, handleProductDecrement }) {
+  return (
+    <div className="productPreview">
+      {tray.map((content) => (
+        <TrayContainer 
+          key={content.id} 
+          content={content} 
+          handleProductIncrement={handleProductIncrement} 
+          handleProductDecrement={handleProductDecrement} 
+        />
+      ))}
+    </div>
+  );
+}
