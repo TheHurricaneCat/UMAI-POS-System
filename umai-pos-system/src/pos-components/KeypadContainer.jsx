de87c36f00ef59746b5c49393727381f17f6c50c
TheHurricaneCat <thc12895nth@gmail.com>
2025-02-11 10:03:55 +0800

    Added total price calculation

--- umai-pos-system/src/pos-components/KeypadContainer.jsx
+++ umai-pos-system/src/pos-components/KeypadContainer.jsx
@@ -1,11 +1,11 @@
 import styles from './KeypadContainer.module.css';
 
-function KeyPadContainer({addNewTray}) {
+function KeyPadContainer({addNewTray, currentTotal}) {
     return (
         <>
             <div className={styles.primaryContainer}>
                 <div className={styles.traySummary}> <h3> Tray 1 Summary </h3> </div>
-                <div className={styles.totalMoney}> <h3> Total: P400.00 </h3> </div>
+                <div className={styles.totalMoney}> <h3> Total: P{currentTotal} </h3> </div>
                 <div className={styles.addTray}> <button onClick={addNewTray}> Add Tray</button> </div>
                 <div className={styles.saveOrder}> <button> Save Order </button> </div>
                 <div className={styles.clearOrder}> <button> Clear Order </button> </div>
