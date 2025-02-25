ac8cdf009dbf33febc7182623cc64104ddf16f3a
Remigaraki <rafdrilon@gmail.com>
2025-02-25 19:38:50 +0800

    Added statistics page import in main page.

--- umai-pos-system/src/main.jsx
+++ umai-pos-system/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './Login.jsx'
+import Statistics from './Statistics.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <App />
   </StrictMode>,
 )
