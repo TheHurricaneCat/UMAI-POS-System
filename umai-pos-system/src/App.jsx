1f6791948677d1fd5e97b101b65f7ea422c3aeea
rysll <cjagaa.jg@gmail.com>
2025-03-17 22:58:11 +0800

    UserContext

--- umai-pos-system/src/App.jsx
+++ umai-pos-system/src/App.jsx
@@ -1,28 +1,23 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css'
+import './App.css';
 
-import {initProductList, appendEntry} from './handlers/DataHandler'
-import {products, modifiers} from './handlers/product.js'
-import useTrayManager from './handlers/UseTrayManagers.js'
+import { initProductList, appendEntry } from './handlers/DataHandler';
+import { products, modifiers } from './handlers/product.js';
+import useTrayManager from './handlers/UseTrayManagers.js';
 
-import ProductCard from './pos-components/ProductCard.jsx'
-import CategoryContainer from './pos-components/CategoryContainer'
-import TrayContainer from './pos-components/TrayContainer.jsx'
-import KeypadContainer from './pos-components/KeypadContainer.jsx'
-import CategoryMenu from './pos-components/CategoryMenu.jsx'
+import ProductCard from './pos-components/ProductCard.jsx';
+import CategoryContainer from './pos-components/CategoryContainer';
+import TrayContainer from './pos-components/TrayContainer.jsx';
+import KeypadContainer from './pos-components/KeypadContainer.jsx';
+import CategoryMenu from './pos-components/CategoryMenu.jsx';
 import PopUp from './global-components/PopUp.jsx';
-import Inventory from './inventory-components/Inventory.jsx'
-
-
-  
+import Inventory from './inventory-components/Inventory.jsx';
 
 import { startSession, getSessionDetails, endSession, saveExcelFile } from './handlers/SessionHandler';
-  function App() {
- 
-  // IMPORTANT NOTE:
-// The following code is meant for the POS system
-// Create new interfaces (i.e. new .jsx components) that will house the other interfaces 
+import { UserContext } from './UserContext';
+
+function App() {
   const {
     tray,
     currentTray,
@@ -43,21 +38,19 @@ import { startSession, getSessionDetails, endSession, saveExcelFile } from './ha
     setCurrentProduct,
     handleScrollToCategory,
     categoryRefs,
-    saveCustomerToTray,  
+    saveCustomerToTray,
   } = useTrayManager();
-
 
   const [startSessionPopup, setStartSessionPopup] = useState(false);
   const [endSessionPopup, setEndSessionPopup] = useState(false);
   const [restoreSessionPopup, setRestoreSessionPopup] = useState(false);
   const [confirm, setConfirm] = useState(false);
+  const { userRole } = useContext(UserContext);
 
   const handleStartSession = async () => {
-    // IMPORTANT CHANGE HERE VVVVVVVVV
-    const result = await startSession(testEmployee); // change the return to specific values for greater user feedback
-    /* console.log("Start Session Result:", await startSession(testEmployee)); */
+    const result = await startSession(testEmployee);
     if (result) {
-      setStartSessionPopup(true); 
+      setStartSessionPopup(true);
     } else {
       setRestoreSessionPopup(true);
     }
@@ -67,7 +60,7 @@ import { startSession, getSessionDetails, endSession, saveExcelFile } from './ha
     const result = await endSession(testEmployee);
     setEndSessionPopup(true);
   };
-    
+
   const navigate = useNavigate();
 
   const handleSaveOrder = async () => {
@@ -87,15 +80,13 @@ import { startSession, getSessionDetails, endSession, saveExcelFile } from './ha
 
   const testEmployee = "TestEmployee2";
 
-	return (
-    // interfaces are invisible containers that hold the components
-    // viewers are containers that displays a list of components (e.g. products, modifiers, trays)
+  return (
     <div className="primaryInterface">
       <div className="logo1"> </div>
       <div className="logo2"> </div>
       <div className="logo3"> </div>
-      <div className="headerButtons"> 
-      <button className="header-button session-start" onClick={handleStartSession}>
+      <div className="headerButtons">
+        <button className="header-button session-start" onClick={handleStartSession}>
           Start Session
         </button>
         <button className="header-button session-end" onClick={handleEndSession}>
@@ -104,76 +95,81 @@ import { startSession, getSessionDetails, endSession, saveExcelFile } from './ha
         <button className="header-button excel" onClick={() => saveExcelFile()}>
           Save Excel
         </button>
-        <button className="header-button inventory" onClick={() => navigate('/inventory')}>
-          Inventory
-        </button>
-        <button className="header-button statistics" onClick={() => navigate('/statistics')}>
-          Statistics
-        </button>
+        {userRole === "admin" && (
+          <>
+            <button className="header-button inventory" onClick={() => navigate('/inventory')}>
+              Inventory
+            </button>
+            <button className="header-button statistics" onClick={() => navigate('/statistics')}>
+              Statistics
+            </button>
+          </>
+        )}
       </div>
-      <PopUp 
-          text={"A new session has started, welcome " + testEmployee}
-          button1={"Confirm"}
-          button2={"Exit"} 
-          trigger={startSessionPopup} 
-          setTrigger={setStartSessionPopup} 
-          confirm={confirm}
-          setConfirm={setConfirm}
+      <PopUp
+        text={"A new session has started, welcome " + testEmployee}
+        button1={"Confirm"}
+        button2={"Exit"}
+        trigger={startSessionPopup}
+        setTrigger={setStartSessionPopup}
+        confirm={confirm}
+        setConfirm={setConfirm}
       />
 
-      <PopUp 
-          text={"Current session has ended"} 
-          button1={"Confirm"}
-          button2={"Exit"} 
-          trigger={endSessionPopup} 
-          setTrigger={setEndSessionPopup} 
-          confirm={confirm}
-          setConfirm={setConfirm}
+      <PopUp
+        text={"Current session has ended"}
+        button1={"Confirm"}
+        button2={"Exit"}
+        trigger={endSessionPopup}
+        setTrigger={setEndSessionPopup}
+        confirm={confirm}
+        setConfirm={setConfirm}
       />
-      <PopUp 
-          text={"A session for " + testEmployee + " is already active. Session has been restored."} 
-          button1={"Confirm"}
-          button2={"Exit"} 
-          trigger={restoreSessionPopup} 
-          setTrigger={setRestoreSessionPopup} 
-          confirm={confirm}
-          setConfirm={setConfirm}
+      <PopUp
+        text={"A session for " + testEmployee + " is already active. Session has been restored."}
+        button1={"Confirm"}
+        button2={"Exit"}
+        trigger={restoreSessionPopup}
+        setTrigger={setRestoreSessionPopup}
+        confirm={confirm}
+        setConfirm={setConfirm}
       />
       <div className="posInterface">
-        <div className="productInterface"> 
-          <CategoryMenu 
-            productList={productList} 
+        <div className="productInterface">
+          <CategoryMenu
+            productList={productList}
             handleScrollToCategory={handleScrollToCategory}
           />
-          <div className="productViewer"> 
+          <div className="productViewer">
             {productList.map((category, index) => (
-              <CategoryContainer 
-              category={category} 
-              type="product" 
-              addToTray={addToTray}
-              categoryRefs={categoryRefs}
-              index={index}/>
+              <CategoryContainer
+                category={category}
+                type="product"
+                addToTray={addToTray}
+                categoryRefs={categoryRefs}
+                index={index}
+              />
             ))}
           </div>
-          <CategoryMenu 
-            productList={modifierList} 
+          <CategoryMenu
+            productList={modifierList}
             handleScrollToCategory={handleScrollToCategory}
           />
           <div className="modifierViewer">
             {modifierList.map((category, index) => (
-              <CategoryContainer 
-                category={category} 
+              <CategoryContainer
+                category={category}
                 type="modifier"
                 addModifier={addModifier}
                 categoryRefs={categoryRefs}
                 index={index}
                 currentProduct={currentProduct}
-                />
+              />
             ))}
           </div>
         </div>
         <div className="trayInterface">
-          <div className="trayHeader"> 
+          <div className="trayHeader">
             <h4> Qty </h4>
             <h4> Img </h4>
             <h4> Item Name </h4>
@@ -181,39 +177,37 @@ import { startSession, getSessionDetails, endSession, saveExcelFile } from './ha
           </div>
           <div className="trayViewer">
             {tray.map((content) => (
-                  <TrayContainer content={content} 
-                  handleProductIncrement={handleProductIncrement} 
-                  handleProductDecrement={handleProductDecrement} 
-                  handleProductDeletion={handleProductDeletion} 
-                  
-                  handleModifierIncrement={handleModifierIncrement} 
-                  handleModifierDecrement={handleModifierDecrement} 
-                  handleModifierDeletion={handleModifierDeletion}
-                  
-                  setCurrentTray={setCurrentTray}
-                  setCurrentProduct={setCurrentProduct}
-                  
-                  currentProduct={currentProduct}
-                  currentTray={currentTray}
-                  saveCustomerToTray={saveCustomerToTray}
-
-                  />
+              <TrayContainer
+                content={content}
+                handleProductIncrement={handleProductIncrement}
+                handleProductDecrement={handleProductDecrement}
+                handleProductDeletion={handleProductDeletion}
+                handleModifierIncrement={handleModifierIncrement}
+                handleModifierDecrement={handleModifierDecrement}
+                handleModifierDeletion={handleModifierDeletion}
+                setCurrentTray={setCurrentTray}
+                setCurrentProduct={setCurrentProduct}
+                currentProduct={currentProduct}
+                currentTray={currentTray}
+                saveCustomerToTray={saveCustomerToTray}
+              />
             ))}
           </div>
-          <div className="keypadViewer"> 
-            <KeypadContainer 
-              addNewTray={addNewTray} 
-              currentTotal={currentTotal} 
-              appendEntry={handleSaveOrder} 
-              tray={tray} 
-              clearTray={clearTray} 
+          <div className="keypadViewer">
+            <KeypadContainer
+              addNewTray={addNewTray}
+              currentTotal={currentTotal}
+              appendEntry={handleSaveOrder}
+              tray={tray}
+              clearTray={clearTray}
               clearCurrentTray={clearCurrentTray}
-              currentTray={currentTray}/>
+              currentTray={currentTray}
+            />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- umai-pos-system/src/main.jsx
+++ umai-pos-system/src/main.jsx
@@ -1,24 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import Login from './Login.jsx'
-import Statistics from './statistics.jsx'
-import Inventory from './inventory-components/Inventory.jsx'
-import AccountPage from './AccountPage.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import Login from './Login.jsx';
+import Statistics from './statistics.jsx';
+import Inventory from './inventory-components/Inventory.jsx';
+import AccountPage from './AccountPage.jsx';
+import { UserProvider } from './UserContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>  {/* Changed from BrowserRouter */}
-       <Routes>
-         <Route path="/" element={<Login />} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/account" element={<AccountPage />} />
-         <Route path="/App" element={<App />} />
-         <Route path="/statistics" element={<Statistics />} />
-         <Route path="/inventory" element={<Inventory />} />
-       </Routes>
-     </HashRouter>
-   </StrictMode>,
-)
+    <UserProvider>
+      <HashRouter>  {/* Changed from BrowserRouter */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/App" element={<App />} />
+          <Route path="/statistics" element={<Statistics />} />
+          <Route path="/inventory" element={<Inventory />} />
+        </Routes>
+      </HashRouter>
+    </UserProvider>
+  </StrictMode>
+);
